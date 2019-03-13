d4c12b01522b317dc8a55f2b15e63bac6a807830
Akash Gupta <42721805+akashggupta@users.noreply.github.com>
2019-03-13 19:08:56 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -17,4 +17,12 @@ par(mfrow=c(2,2))
 
 plot(subpower$Time,as.numeric(subpower$Global_active_power),type="l",  xlab="",ylab="Global Active Power")
 plot(subpower$Time,as.numeric(as.character(subpower$Voltage)), type="l",xlab="datetime",ylab="Voltage")
+plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
  
+ plot(subpower$Time,as.numeric(as.character(subpower$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
+
+dev.off()
