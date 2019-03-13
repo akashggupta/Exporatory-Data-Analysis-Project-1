2fa54b6ce9fea245d90caa853d42113c0e59329d
Akash Gupta <42721805+akashggupta@users.noreply.github.com>
2019-03-13 18:53:10 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -10,7 +10,7 @@ subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 #to save in png format
-png("plot2.R")
+png("plot2.png")
 # calling the basic plot function
 plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 
